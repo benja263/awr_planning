c41d614b7e87f605d0e6e2bf2b882d6e03375b9a
benja263 <benjafuhrer@gmail.com>
2023-07-31 17:36:16 +0300

    fixed cat

--- policies/actor_critic_ts.py
+++ policies/actor_critic_ts.py
@@ -248,7 +248,7 @@ class ActorCriticCnnTSPolicy(ActorCriticCnnPolicyDepth0):
             all_first_actions.append(first_action)
         all_rewards_th = th.cat(all_rewards).reshape([-1, 1])
         val_coef = self.cule_bfs.gamma ** self.cule_bfs.max_depth
-        cat_features = self.extract_features(th.cat(all_leaves_obs))
+        cat_features = self.extract_features(th.cat(all_leaves_obs, dim=0))
         shared_features = self.mlp_extractor.shared_net(cat_features)
         if self.use_leaves_v:
             latent_vf_root = self.mlp_extractor.value_net(shared_features)
