32fc469cd0115be84b66eadb83bb8ce6ddec90b6
benja263 <benjafuhrer@gmail.com>
2023-07-31 18:38:53 +0300

    bug

--- policies/actor_critic_ts.py
+++ policies/actor_critic_ts.py
@@ -99,8 +99,10 @@ class ActorCriticCnnTSPolicy(ActorCriticCnnPolicyDepth0):
         actions = distribution.get_actions(deterministic=deterministic)
         log_prob = distribution.log_prob(actions)
         if self.time_step - self.buffer_size in self.timestep2obs_dict:
-            del self.obs2leaves_dict[self.timestep2obs_dict[self.time_step - self.buffer_size]]
-            del self.obs2timestep_dict[self.timestep2obs_dict[self.time_step - self.buffer_size]]
+            if self.time_step - self.buffer_size in self.obs2leaves_dict: 
+                del self.obs2leaves_dict[self.timestep2obs_dict[self.time_step - self.buffer_size]]
+            if self.time_step - self.buffer_size in self.obs2timestep_dict: 
+                del self.obs2timestep_dict[self.timestep2obs_dict[self.time_step - self.buffer_size]]
             del self.timestep2obs_dict[self.time_step - self.buffer_size]
         self.time_step += 1
         return actions, value_root, log_prob
