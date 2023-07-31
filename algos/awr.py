d68bf54b1f4cfb752153b92f4510737e288c5217
benja263 <benjafuhrer@gmail.com>
2023-07-31 16:23:44 +0300

    progress bar deletion

--- algos/awr.py
+++ algos/awr.py
@@ -221,7 +221,6 @@ class AWR(OffPolicyAlgorithm):
         log_interval: int = 100,
         tb_log_name: str = "",
         reset_num_timesteps: bool = True,
-        progress_bar: bool = False,
     ):
         return super().learn(
             total_timesteps=total_timesteps,
@@ -229,7 +228,6 @@ class AWR(OffPolicyAlgorithm):
             log_interval=log_interval,
             tb_log_name=tb_log_name,
             reset_num_timesteps=reset_num_timesteps,
-            progress_bar=progress_bar
         )
     
 
