f0bcecd78d070fd96e1779a253e69fa3c0185f85
benja263 <benjafuhrer@gmail.com>
2023-10-02 10:31:59 +0300

    fds

--- policies/actor_critic_ts.py
+++ policies/actor_critic_ts.py
@@ -52,13 +52,13 @@ class ActorCriticCnnTSPolicy(ActorCriticCnnPolicyDepth0):
         hash_obs = self.hash_obs(obs)[0].item()
         if hash_obs in self.obs2leaves_dict:
             leaves_observations, rewards, first_action = self.obs2leaves_dict.get(hash_obs)
-            # leaves_observations, rewards, first_action = leaves_observations.to(obs.device), rewards.to(obs.device),  first_action if first_action is None else first_action.to(obs.device)
+            leaves_observations, rewards, first_action = leaves_observations.to(obs.device), rewards.to(obs.device),  first_action if first_action is None else first_action.to(obs.device)
             if hash_obs in self.timestep2obs_dict:
                 del self.timestep2obs_dict[self.obs2timestep_dict[hash_obs]]
         else:
             leaves_observations, rewards, first_action = self.cule_bfs.bfs(obs, self.cule_bfs.max_depth)
-            # self.obs2leaves_dict[hash_obs] = leaves_observations.cpu(), rewards.cpu(), first_action if first_action is None else first_action.cpu()
-            self.obs2leaves_dict[hash_obs] = leaves_observations, rewards, first_action
+            self.obs2leaves_dict[hash_obs] = leaves_observations.cpu(), rewards.cpu(), first_action if first_action is None else first_action.cpu()
+            # self.obs2leaves_dict[hash_obs] = leaves_observations, rewards, first_action
         self.obs2timestep_dict[hash_obs] = self.time_step
         self.timestep2obs_dict[self.time_step] = hash_obs
         # Preprocess the observation if needed
@@ -136,12 +136,12 @@ class ActorCriticCnnTSPolicy(ActorCriticCnnPolicyDepth0):
             hash_obs = hash_obses[i].item()
             if hash_obs in self.obs2leaves_dict:
                 leaves_observations, rewards, first_action = self.obs2leaves_dict.get(hash_obs)
-                # leaves_observations, rewards, first_action = leaves_observations.to(obs.device), rewards.to(obs.device),  first_action if first_action is None else first_action.to(obs.device)
+                leaves_observations, rewards, first_action = leaves_observations.to(obs.device), rewards.to(obs.device),  first_action if first_action is None else first_action.to(obs.device)
             else:
                 print("This should not happen! observation not in our dictionary")
                 leaves_observations, rewards, first_action = self.cule_bfs.bfs(obs[i], self.cule_bfs.max_depth)
-                self.obs2leaves_dict[hash_obs] = leaves_observations, rewards, first_action
-                # self.obs2leaves_dict[hash_obs] = leaves_observations.cpu(), rewards.cpu(), first_action if first_action is None else first_action.cpu()
+                # self.obs2leaves_dict[hash_obs] = leaves_observations, rewards, first_action
+                self.obs2leaves_dict[hash_obs] = leaves_observations.cpu(), rewards.cpu(), first_action if first_action is None else first_action.cpu()
             all_leaves_obs.append(leaves_observations)
             all_rewards.append(rewards)
             all_first_actions.append(first_action)
@@ -242,12 +242,12 @@ class ActorCriticCnnTSPolicy(ActorCriticCnnPolicyDepth0):
             hash_obs = hash_obses[i].item()
             if hash_obs in self.obs2leaves_dict:
                 leaves_observations, rewards, _ = self.obs2leaves_dict.get(hash_obs)
-                # leaves_observations, rewards = leaves_observations.to(obs.device), rewards.to(obs.device)
+                leaves_observations, rewards = leaves_observations.to(obs.device), rewards.to(obs.device)
             else:
                 leaves_observations, rewards, first_action = self.cule_bfs.bfs(obs[i], self.cule_bfs.max_depth)
                 # first_action = first_action if first_action is None else first_action.cpu()
-                # self.obs2leaves_dict[hash_obs] = leaves_observations.cpu(), rewards.cpu(), first_action if first_action is None else first_action.cpu()
-                self.obs2leaves_dict[hash_obs] = leaves_observations, rewards, first_action
+                self.obs2leaves_dict[hash_obs] = leaves_observations.cpu(), rewards.cpu(), first_action if first_action is None else first_action.cpu()
+                # self.obs2leaves_dict[hash_obs] = leaves_observations, rewards, first_action
                 self.obs2timestep_dict[hash_obs] = self.time_step
                 self.timestep2obs_dict[self.time_step] = hash_obs
                 self.time_step += 1
